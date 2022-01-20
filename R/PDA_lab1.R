6bb7d2334f0809ed2c400cddea5897e5ffb759be
Кирилл Гулиев <guliev@ecopsy.ru>
2022-01-20 15:28:40 +0300

    pvalue calculated

--- R/PDA_lab1.R
+++ R/PDA_lab1.R
@@ -27,7 +27,9 @@ library(ggplot2)
 ggplot(data, aes(tails, prob)) + 
   geom_bar(stat = "identity", colour = "red", fill = "yellow") +
   geom_text(aes(label=round(prob, 2))) +
-  scale_x_continuous(breaks=seq(0,10,1))
+  scale_x_continuous(breaks=seq(0,10,1)) +
+  ggtitle("Binomial Distribution") + 
+  ggthemes::theme_fivethirtyeight()
 
 # let us highlight a p-value part of the plot 
 data$region = ifelse(data$tails > 7, "pvalue", "confidence")
@@ -37,7 +39,16 @@ ggplot(data, aes(tails, prob, fill = region)) +
   geom_bar(stat = "identity") +
   geom_text(aes(label=round(prob, 2))) +
   scale_x_continuous(breaks=seq(0,10,1)) + 
-  scale_fill_manual(values = c("pvalue" = "red", "confidence" = "green"))
+  scale_fill_manual(values = c("pvalue" = "red", "confidence" = "green")) +
+  scale_x_continuous(breaks=seq(0,10,1)) +
+  ggtitle("Binomial Distribution", subtitle = "With rejection region") + 
+  ggthemes::theme_fivethirtyeight()
 
 # calculate p-value
-pvalue 
+
+# Let us toss a coin 10 times. As a result, you get 8 tails and 2
+# heads. Test the null hypothesis that our coin is fair against the
+# alternative P(tails) > P(heads) by using p-value.
+
+pvalue <- dbinom(8, 10, 0.5) + dbinom(9, 10, 0.5) + dbinom(10, 10, 0.5)
+pvalue
